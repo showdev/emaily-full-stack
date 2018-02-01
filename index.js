9b1c7372ad1882829fc0fbaf4dacc411ebeb3b1c
Oleksij Shovgenia <ashovgenja@salsalabs.com>
2018-02-01 12:27:51 +0200

    test

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const express = require('express')
 const app = express()
 
 const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send({"HI": "THERE"})
+})
+
 app.listen(port || 5000)
