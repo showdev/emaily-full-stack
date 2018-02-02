fc822f55c05d0b7f78666ac5d1c379a510a0d862
Oleksij Shovgenia <ashovgenja@salsalabs.com>
2018-02-02 16:07:01 +0200

    prepare heroku prod

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
-const googleClientID = 'process.env.GOOGLE_CLIENT_ID'
+const googleClientID = process.env.GOOGLE_CLIENT_ID
 const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET
 const facebookAppId = process.env.FACEBOOK_APP_ID
 const facebookAppSecret = process.env.FACEBOOK_APP_SECRET
