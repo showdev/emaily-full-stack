046f37d5f0c449c127598624e4ee945813d2485d
Oleksij Shovgenia <ashovgenja@salsalabs.com>
2018-02-02 16:55:22 +0200

    add tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -36,6 +36,7 @@ passport.use(
       clientID: googleClientID,
       clientSecret: googleClientSecret,
       callbackURL: '/auth/google/callback',
+      proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ googleId: profile.id })
