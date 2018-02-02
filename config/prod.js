f880e451baaaf612e1437ced6bc9c0d57977d899
Oleksij Shovgenia <ashovgenja@salsalabs.com>
2018-02-02 15:29:09 +0200

    fix typo keys

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 const googleClientID = 'process.env.GOOGLE_CLIENT_ID'
-const googleClientSecret = 'process.env.GOOGLE_CLIENT_SECRET'
-const facebookAppId = 'process.env.FACEBOOK_APP_ID'
-const facebookAppSecret = 'process.env.FACEBOOK_APP_SECRET'
-const cookieKey = 'process.env.COOKIE_KEY'
+const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET
+const facebookAppId = process.env.FACEBOOK_APP_ID
+const facebookAppSecret = process.env.FACEBOOK_APP_SECRET
+const cookieKey = process.env.COOKIE_KEY
 
 module.exports = {
   googleClientID,
