064427608fff297065b2e00fb84a67f2e619d4ed
Oleksij Shovgenia <ashovgenja@salsalabs.com>
2018-02-02 16:15:56 +0200

    debug

--- config/prod.js
+++ config/prod.js
@@ -4,6 +4,15 @@ const facebookAppId = process.env.FACEBOOK_APP_ID
 const facebookAppSecret = process.env.FACEBOOK_APP_SECRET
 const cookieKey = process.env.COOKIE_KEY
 
+console.log(
+  'KEYS: ',
+  googleClientID,
+  googleClientSecret,
+  facebookAppId,
+  facebookAppSecret,
+  cookieKey
+)
+
 module.exports = {
   googleClientID,
   googleClientSecret,
